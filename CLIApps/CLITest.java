c92a0e3e2a63ed631043ef21c6c3b1ab8f7001b0
Ryan <maloney.rpa@gmail.com>
2019-10-17 12:25:21 +0100

    auto pushed via batch

--- CLIApps/CLITest.java
+++ CLIApps/CLITest.java
@@ -1,5 +1,9 @@
+import java.util.*;
 class CLITest{
     public static void main(String args[]){
+
+        Scanner usrn = new Scanner(System.in);
+        Scanner passw = new Scanner(System.in);
         System.out.println("Welcome to CLI");
         try{
         Thread.sleep(2000);//2000ms = 2s
@@ -7,5 +11,9 @@ class CLITest{
         catch(InterruptedException ex){
         };
         System.out.println("Please enter your login details:");
+        System.out.print("Enter your username: ");
+        String Username = usrn.next();
+        System.out.print("Enter your Passcode ####: ");
+        String Password = passw.next();
     }
 }
